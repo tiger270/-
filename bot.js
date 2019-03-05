092df64878363d7d9f509dae3fc07ffa8b6cd74d
Mehdi0457 <48065343+Mehdi0457@users.noreply.github.com>
2019-03-05 17:27:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2138,7 +2138,7 @@ client.on("guildMemberAdd", member => {
           .setColor('RANDOM')
           .setThumbnail(h.avatarURL)
           .setAuthor(h.username,h.avatarURL)
-          .addField(': تاريخ دخولك الدسكورد',${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **n**${moment(member.user.createdAt).fromNow()},true)            
+          .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **n**${moment(member.user.createdAt).fromNow()},true)            
            .addField(': تاريخ دخولك السيرفر',`${moment(member.joinedAt).format('D/M/YYYY h:mm a ')} n${moment(member.joinedAt).startOf(' ').fromNow()}``, true) 
            .setFooter(${h.tag},"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
        welcomer.send({embed:norelden});          
